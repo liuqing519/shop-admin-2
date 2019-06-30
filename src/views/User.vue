01a2e43edfd0ff24185578375d51f0620a290575
liuyanxia <394543264@qq.com>
2019-06-30 20:33:44 +0800

    分页功能完成了

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,34 +1,47 @@
 <template>
-  <el-table :data="tableList" stripe style="width: 100%">
-    <el-table-column prop="username" label="姓名" width="180"></el-table-column>
-    <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
-    <el-table-column prop="mobile" label="电话"></el-table-column>
-    <el-table-column label="用户状态">
-      <!-- 作用域插槽 -->
-      <template v-slot="{row}">
-        <!-- {{row}} -->
-        <el-switch v-model="row.type" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <el-button type="primary" icon="el-icon-edit" plain size='mini'></el-button>
-      <el-button type="danger" icon="el-icon-delete" plain size='mini'></el-button>
-<el-button type="success" icon="el-icon-check" plain size='mini'>分配角色</el-button>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-table :data="tableList" stripe style="width: 100%">
+      <el-table-column prop="username" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+      <el-table-column prop="mobile" label="电话"></el-table-column>
+      <el-table-column label="用户状态">
+        <!-- 作用域插槽 -->
+        <template v-slot="{row}">
+          <!-- {{row}} -->
+          <el-switch v-model="row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
+        <el-button type="danger" icon="el-icon-delete" plain size="mini"></el-button>
+        <el-button type="success" icon="el-icon-check" plain size="mini">分配角色</el-button>
+      </el-table-column>
+    </el-table>
+
+    <!-- 分页器 -->
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="total"
+      :page-size="pagesize"
+      :current-page="currentPage"
+      @current-change="onPageChange"
+    ></el-pagination>
+  </div>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 export default {
   data() {
     return {
-      tableList: [
-        
-      ]
+      tableList: [],
+      total: 1,
+      pagesize: 3,
+      currentPage: 1
     };
   },
-  async created() {
+  created() {
     // axios({
     //   url: 'http://localhost:8888/api/private/v1/users',
     //   params: {
@@ -42,18 +55,28 @@ export default {
     //   this.tableList = data.users
     // })
 
-    let result = await axios({
-      url: 'http://localhost:8888/api/private/v1/users',
-      params: {
-        pagenum: 1,
-        pagesize: 5
-      },
-      headers: {
-        "Authorization": localStorage.getItem('token')
-      }
-    })
-    // console.log(result)
-    this.tableList = result.data.data.users
+    this.getUserList();
+  },
+  methods: {
+    onPageChange(page) {
+      this.currentPage = page
+      this.getUserList()
+    },
+    async getUserList() {
+      let res = await axios({
+        url: "http://localhost:8888/api/private/v1/users",
+        params: {
+          pagenum: this.currentPage,
+          pagesize: this.pagesize
+        },
+        headers: {
+          Authorization: localStorage.getItem("token")
+        }
+      });
+
+      this.tableList = res.data.data.users;
+      this.total = res.data.data.total;
+    }
   }
 };
 </script>
