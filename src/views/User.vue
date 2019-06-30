056151870f376426e83624a474c8b42f40951f9f
liuyanxia <394543264@qq.com>
2019-06-30 23:16:39 +0800

    添加用户功能完成

--- src/views/User.vue
+++ src/views/User.vue
@@ -8,14 +8,40 @@
 
     <el-row :gutter="20">
       <el-col :span="8">
-        <el-input placeholder="请输入搜索内容" class="input-with-select">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+        <el-input
+          placeholder="请输入搜索内容"
+          class="input-with-select"
+          v-model="query"
+          @keyup.enter.native="search"
+        >
+          <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
         </el-input>
       </el-col>
       <el-col :span="8">
-        <el-button type="success" plain>添加用户</el-button>
+        <el-button type="success" plain @click="addUser">添加用户</el-button>
       </el-col>
     </el-row>
+
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
+      <el-form :model="ruleForm" label-width="100px" :rules="rules" ref="ruleForm">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm.username" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input auto-complete="off" v-model="ruleForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="ruleForm.email" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="mobile">
+          <el-input v-model="ruleForm.mobile" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="resetForm('ruleForm')">取 消</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
+      </div>
+    </el-dialog>
 
     <el-table :data="tableList" stripe style="width: 100%">
       <el-table-column prop="username" label="姓名" width="180"></el-table-column>
@@ -60,7 +86,49 @@ export default {
       tableList: [],
       total: 1,
       pagesize: 3,
-      currentPage: 1
+      currentPage: 1,
+      query: "",
+      dialogFormVisible: false,
+      ruleForm: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 5,
+            max: 12,
+            message: "长度在 5 到 12 个字符",
+            trigger: "change"
+          }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "change"
+          }
+        ],
+        email: [
+          {
+            pattern: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+            message: "请输入正确的邮箱格式",
+            trigger: "change"
+          }
+        ],
+        mobile: [
+          {
+            pattern: /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+            message: "请输入正确的手机格式",
+            trigger: "change"
+          }
+        ]
+      }
     };
   },
   created() {
@@ -87,7 +155,7 @@ export default {
       this.total = res.data.data.total;
     },
     toggleState(user) {
-      console.log(user);
+      // console.log(user);
       axios({
         url: `http://localhost:8888/api/private/v1/users/${user.id}/state/${user.mg_state}`,
         method: "put",
@@ -98,10 +166,53 @@ export default {
         if (res.data.meta.status === 200) {
           this.$message({
             message: "恭喜你，状态修改成功",
-            type: "success"
+            type: "success",
+            duration: 1000
           });
         }
       });
+    },
+    async search() {
+      let res = await axios({
+        url: `http://localhost:8888/api/private/v1/users`,
+        params: {
+          query: this.query,
+          pagenum: this.currentPage,
+          pagesize: this.pagesize
+        },
+        headers: {
+          Authorization: localStorage.getItem("token")
+        }
+      });
+      this.tableList = res.data.data.users;
+      this.total = res.data.data.total;
+    },
+    addUser() {
+      this.dialogFormVisible = true;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios({
+            url: 'http://localhost:8888/api/private/v1/users',
+            method: 'post',
+            data: this.ruleForm,
+            headers: {
+              Authorization: localStorage.getItem("token")
+            }
+          }).then( res => {
+            this.getUserList()
+            this.dialogFormVisible = false
+            // this.ruleForm = ''
+          })
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
 };
